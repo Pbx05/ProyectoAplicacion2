fc38e4e78f176eb4d92e1575b84aa98cf3046fbf
PabloAparicioNoya <apanoyapan@gmail.com>
2024-10-02 18:11:47 +0200

    fallo en pull

--- src/modelo/Modelo.java
+++ src/modelo/Modelo.java
@@ -12,6 +12,7 @@ public class Modelo {
 	private Vista[] misVistas;
 	private Controlador miControlador;
 	private File usuarios;
+	File directorio;
 
 	public void setControlador(Controlador miControlador) {
 		this.miControlador = miControlador;
@@ -19,6 +20,21 @@ public class Modelo {
 
 	public void setVista(Vista[] misVistas) {
 		this.misVistas = misVistas;
+	}
+	
+	public Object[][] visualizarDatos() {
+		directorio = new File("./src/Carpeta");
+		File[] archivos = directorio.listFiles();
+		Object[][] datos = new Object[archivos.length][2];
+		if (archivos != null) {
+			int i = 0;
+			for (File archivo : archivos) {
+				datos[i][0] = archivo.getName();
+				datos[i][1] = archivo.length();
+				i++;
+			}
+		}
+		return datos;
 	}
 
 	public boolean serializarEmpleado(String nombre, String dni, int edad, double sueldo, String genero) {
