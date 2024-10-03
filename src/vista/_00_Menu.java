44284368c52e05c7d79563b38f0ed8803a69038a
PabloAparicioNoya <apanoyapan@gmail.com>
2024-10-03 18:45:15 +0200

    Front-end de la vista menu acabado

--- src/vista/_00_Menu.java
+++ src/vista/_00_Menu.java
@@ -1,24 +1,27 @@
 package vista;
 
-import java.awt.EventQueue;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.border.BevelBorder;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 
 import controlador.Controlador;
 import modelo.Modelo;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.JLabel;
-import java.awt.Font;
-import javax.swing.JTextField;
-import javax.swing.ListSelectionModel;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
 
 public class _00_Menu extends JFrame implements Vista {
 
@@ -34,9 +37,10 @@ public class _00_Menu extends JFrame implements Vista {
 	private JButton btnComprimirArchivo;
 	private JButton btnDescomprimirArchivo;
 	private JButton btnEliminarArchivo;
-	private JButton btnVisualizarArchivo;
 	private JScrollPane scrollPane;
 	private Object[][] datos;
+	private JLabel lblFondo;
+	private JLabel lblFondoMandos;
 
 	public void setModelo(Modelo miModelo) {
 		this.miModelo = miModelo;
@@ -56,12 +60,20 @@ public class _00_Menu extends JFrame implements Vista {
 		setBounds(100, 100, 713, 470);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		lblFondoMandos = new JLabel("New label");
+		lblFondoMandos.setBounds(339, 230, 360, 203);
+		contentPane.add(lblFondoMandos);
+		ImageIcon imagenMandos = new ImageIcon("./src/Assets/Mandos.png");
+		Image imagenEscalada = imagenMandos.getImage().getScaledInstance(lblFondoMandos.getWidth(), lblFondoMandos.getHeight(), Image.SCALE_SMOOTH);
+		lblFondoMandos.setIcon(new ImageIcon(imagenEscalada));
+
+		
 
 		scrollPane = new JScrollPane();
-		scrollPane.setBounds(317, 0, 382, 433);
+		scrollPane.setBounds(339, 0, 360, 220);
 		contentPane.add(scrollPane);
 
 		tableDatos = new JTable();
@@ -69,23 +81,34 @@ public class _00_Menu extends JFrame implements Vista {
 		tableDatos.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
 
 		JLabel lblNewLabel = new JLabel("Gestor de archivos avanzado");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setForeground(new Color(255, 255, 255));
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
-		lblNewLabel.setBounds(10, 11, 307, 18);
+		lblNewLabel.setBounds(10, 11, 329, 18);
 		contentPane.add(lblNewLabel);
 
 		btnAgregarArchivo = new JButton("Agregar Archivo");
+		btnAgregarArchivo.setForeground(new Color(255, 255, 255));
 		btnAgregarArchivo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				miControlador.cambiarVentana(0, 1);
 			}
 		});
-		btnAgregarArchivo.setBounds(10, 145, 172, 23);
+		btnAgregarArchivo.setBackground(new Color(128, 0, 128)); 
+		// Da relieve al borde del boton
+		btnAgregarArchivo.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED, Color.GRAY, Color.DARK_GRAY));
+		btnAgregarArchivo.setFocusPainted(false);
+		// Rellena el area del boton
+		btnAgregarArchivo.setContentAreaFilled(true);
+		// Para dar opacidad al fondo
+		btnAgregarArchivo.setOpaque(true);
+		btnAgregarArchivo.setBounds(443, 255, 172, 23);
 		contentPane.add(btnAgregarArchivo);
 
-		btnDeserializarArchivo = new JButton("Deserializar Archivo");
+		btnDeserializarArchivo = new JButton("");
 		btnDeserializarArchivo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(miControlador.recogerDatosDeserializarEmpleado()) {
+				if (miControlador.recogerDatosDeserializarEmpleado()) {
 					datos = miControlador.visualizarDatos();
 
 					String[] columnas = { "Nombre", "Tamaño", "Estado" };
@@ -94,28 +117,43 @@ public class _00_Menu extends JFrame implements Vista {
 				}
 			}
 		});
-		btnDeserializarArchivo.setBounds(10, 179, 172, 23);
+		btnDeserializarArchivo.setBackground(new Color(255, 204, 0));  
+		btnDeserializarArchivo.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED, Color.GRAY, Color.DARK_GRAY)); 
+		btnDeserializarArchivo.setFocusPainted(false);  
+		btnDeserializarArchivo.setContentAreaFilled(true); 
+		btnDeserializarArchivo.setOpaque(true);
+		btnDeserializarArchivo.setBounds(239, 95, 54, 48);
 		contentPane.add(btnDeserializarArchivo);
 
-		btnComprimirArchivo = new JButton("Comprimir archivo");
+		btnComprimirArchivo = new JButton("");
 		btnComprimirArchivo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 
 			}
 		});
-		btnComprimirArchivo.setBounds(10, 214, 172, 23);
+		btnComprimirArchivo.setBackground(new Color(0, 102, 204));  // Color azul brillante
+		btnComprimirArchivo.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED, Color.GRAY, Color.DARK_GRAY)); // Borde en relieve
+		btnComprimirArchivo.setFocusPainted(false);  // Evita el borde de enfoque
+		btnComprimirArchivo.setContentAreaFilled(true); // Para rellenar el área del botón
+		btnComprimirArchivo.setOpaque(true);  // Para que el fondo sea opaco
+		btnComprimirArchivo.setBounds(239, 170, 54, 48);
 		contentPane.add(btnComprimirArchivo);
 
-		btnDescomprimirArchivo = new JButton("Descomprimir archivo");
+		btnDescomprimirArchivo = new JButton("");
 		btnDescomprimirArchivo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 
 			}
 		});
-		btnDescomprimirArchivo.setBounds(10, 248, 172, 23);
+		btnDescomprimirArchivo.setBackground(new Color(0, 204, 0));
+		btnDescomprimirArchivo.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED, Color.GRAY, Color.DARK_GRAY)); 
+		btnDescomprimirArchivo.setFocusPainted(false);
+		btnDescomprimirArchivo.setContentAreaFilled(true);
+		btnDescomprimirArchivo.setOpaque(true);
+		btnDescomprimirArchivo.setBounds(239, 239, 54, 48);
 		contentPane.add(btnDescomprimirArchivo);
 
-		btnEliminarArchivo = new JButton("Eliminar archivo");
+		btnEliminarArchivo = new JButton("");
 		btnEliminarArchivo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				miControlador.cogerDatosEliminarEmpleado();
@@ -126,22 +164,51 @@ public class _00_Menu extends JFrame implements Vista {
 
 			}
 		});
-		btnEliminarArchivo.setBounds(10, 282, 172, 23);
+		btnEliminarArchivo.setBackground(new Color(255, 0, 0));
+		btnEliminarArchivo.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED, Color.GRAY, Color.DARK_GRAY));
+		btnEliminarArchivo.setFocusPainted(false); 
+		btnEliminarArchivo.setContentAreaFilled(true); 
+		btnEliminarArchivo.setOpaque(true);  
+		btnEliminarArchivo.setBounds(239, 309, 54, 48);
 		contentPane.add(btnEliminarArchivo);
 
-		btnVisualizarArchivo = new JButton("Visualizar archivo");
-		btnVisualizarArchivo.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				datos = miControlador.visualizarDatos();
+		ImageIcon iconoOriginal = new ImageIcon("./src/Assets/RefreshDef.png");
+		Image imagenRedimensionada = iconoOriginal.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);
+		ImageIcon iconoRedimensionado = new ImageIcon(imagenRedimensionada);
+		
+		JLabel lblDeserializarArchivo = new JLabel("Deserializar ");
+		lblDeserializarArchivo.setForeground(new Color(255, 255, 255));
+		lblDeserializarArchivo.setFont(new Font("Sitka Heading", Font.BOLD, 20));
+		lblDeserializarArchivo.setBounds(80, 111, 127, 23);
+		contentPane.add(lblDeserializarArchivo);
+		
+		JLabel lblComprimirArchivo = new JLabel("Comprimir");
+		lblComprimirArchivo.setForeground(new Color(255, 255, 255));
+		lblComprimirArchivo.setFont(new Font("Sitka Heading", Font.BOLD, 20));
+		lblComprimirArchivo.setBounds(80, 187, 127, 23);
+		contentPane.add(lblComprimirArchivo);
+		
+		JLabel lblDescomprimirArchivo = new JLabel("Descomprimir");
+		lblDescomprimirArchivo.setForeground(new Color(255, 255, 255));
+		lblDescomprimirArchivo.setFont(new Font("Sitka Heading", Font.BOLD, 20));
+		lblDescomprimirArchivo.setBounds(80, 256, 127, 23);
+		contentPane.add(lblDescomprimirArchivo);
+		
+		JLabel lblEliminarArchivo = new JLabel("Eliminar");
+		lblEliminarArchivo.setForeground(new Color(255, 255, 255));
+		lblEliminarArchivo.setFont(new Font("Sitka Heading", Font.BOLD, 20));
+		lblEliminarArchivo.setBounds(80, 327, 127, 23);
+		contentPane.add(lblEliminarArchivo);
+		
+		
+		ImageIcon imagen = new ImageIcon("./src/Assets/FondoEspacio.jpg");
 
-				String[] columnas = { "Nombre", "Tamaño", "Estado" };
-				modelo = new DefaultTableModel(datos, columnas);
-				tableDatos.setModel(modelo);
-			}
-		});
-		btnVisualizarArchivo.setBounds(10, 316, 172, 23);
-		contentPane.add(btnVisualizarArchivo);
-
+		lblFondo = new JLabel(imagen);
+		lblFondo.setBounds(0, 0, 699, 433);
+		contentPane.add(lblFondo);
+		lblFondo.setIcon(imagen);
+		
+		
 		// Label para poder poner una imagen de fondo
 //		lblImagenPrincipio = new JLabel("New label");
 //		lblImagenPrincipio.setBounds(-36, -45, 367, 328);
